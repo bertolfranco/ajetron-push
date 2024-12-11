c8c5c4df1ed1fef169af1a9e6b28fabfce7ec239
Miguel Juarez <mjuarez@sigcomt.com>
2024-12-11 14:26:35 -0500

    cambios

---
+++ comisiones/comisiones_static_files.php
@@ -0,0 +1,7 @@
+<ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="static/8_plantilla_modelos_compensacion.csv">Modelo Compensacion</a></li>
+                                    <li><a class="dropdown-item" href="static/9_plantilla_tipo_comision.csv">Tipo Comision</a></li>
+                                    <li><a class="dropdown-item" href="static/10_plantilla_objetivo_cobertura.csv">Objetivo Cobertura</a></li>
+                                    <li><a class="dropdown-item" href="static/11_plantilla_fechas_habiles.csv">Fechas Habiles</a></li>
+                                    <li><a class="dropdown-item" href="static/12_plantilla_ipp_actual.csv">Ipp Actual</a></li>
+                                </ul>
