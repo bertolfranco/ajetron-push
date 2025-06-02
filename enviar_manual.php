a2e5e19686bb2c57f471897c6b9bc365db8cad1a
bertol26 <bertolfranco26@gmail.com>
2025-06-02 12:08:21 -0500

    avance

--- enviar_manual.php
+++ enviar_manual.php
@@ -26,11 +26,15 @@ $token = $TOKEN_BOT;
 
 $res ="select DISTINCT pc.*, vpb.idtelegram 
 from push_carga pc 
-left join v_push_base vpb on pc.pais=vpb.pais AND (
-        (pc.sucursal IS NOT NULL AND pc.sucursal = vpb.cod_sucursal) OR
-        (pc.zona     IS NOT NULL AND pc.zona     = vpb.zona) OR
-        (pc.ruta     IS NOT NULL AND pc.ruta     = vpb.ruta)
-    )
+join v_push_base vpb on pc.pais=vpb.pais
+AND ((
+      (pc.sucursal IS NULL OR pc.sucursal = '')
+      AND (pc.zona IS NULL OR pc.zona = '')
+	  AND (pc.ruta IS NULL OR pc.ruta = ''))
+        OR
+        ((pc.sucursal IS NOT NULL AND pc.sucursal != '' AND pc.sucursal = vpb.cod_sucursal)
+            OR (pc.zona     IS NOT NULL AND pc.zona     != '' AND pc.zona = vpb.zona)
+            OR (pc.ruta     IS NOT NULL AND pc.ruta     != '' AND pc.ruta = vpb.ruta)))
 where pc.id  = '$id'
 ";
 
