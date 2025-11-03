6a9de91e260b21c53187bcf94768e2b645bac38c
bertol26 <bertolfranco26@gmail.com>
2025-11-03 17:33:21 -0500

    avance

--- enviar_manual.php
+++ enviar_manual.php
@@ -17,15 +17,32 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('America/Guatemala');
 
-if(!isset($_POST['id_camp'])){
+if (!isset($_POST['id_camp'])) {
     throw new Exception('id de campaña requerido');
 }
-$id=$_POST['id_camp'];
+$id = $_POST['id_camp'];
 
 $token = $TOKEN_BOT;
 
-$res ="select DISTINCT pc.*, vpb.idtelegram 
-from push_carga pc 
+$username = $_SESSION["username"];
+
+if ($username == "admin-ECONORED") {
+    $res = "select DISTINCT pc.*, vpb.idtelegram
+from push_carga pc
+join v_push_base_econored vpb on pc.pais=vpb.pais
+AND ((
+      (pc.sucursal IS NULL OR pc.sucursal = '')
+      AND (pc.zona IS NULL OR pc.zona = '')
+	  AND (pc.ruta IS NULL OR pc.ruta = ''))
+        OR
+        ((pc.sucursal IS NOT NULL AND pc.sucursal != '' AND pc.sucursal = vpb.cod_sucursal)
+            OR (pc.zona     IS NOT NULL AND pc.zona     != '' AND pc.zona = vpb.zona)
+            OR (pc.ruta     IS NOT NULL AND pc.ruta     != '' AND pc.ruta = vpb.ruta)))
+where pc.id  = '$id'
+";
+} else {
+    $res = "select DISTINCT pc.*, vpb.idtelegram
+from push_carga pc
 join v_push_base vpb on pc.pais=vpb.pais
 AND ((
       (pc.sucursal IS NULL OR pc.sucursal = '')
@@ -37,42 +54,43 @@ AND ((
             OR (pc.ruta     IS NOT NULL AND pc.ruta     != '' AND pc.ruta = vpb.ruta)))
 where pc.id  = '$id'
 ";
+}
 
 $res = $mysqli->query("$res");
 $fila = $res->fetch_all(MYSQLI_ASSOC);
 
-foreach ($fila as  $value) {
+foreach ($fila as $value) {
 
-    if($value['tipo']=='imagen'){
+    if ($value['tipo'] == 'imagen') {
         $datos = [
             'chat_id' => $value['idtelegram'],
             //'chat_id' => 538709214,
             'photo' => $value['valor'],
             'caption' => $value['etiqueta']
         ];
-    }elseif($value['tipo']=='texto'){
+    } elseif ($value['tipo'] == 'texto') {
         $escapedMessage = str_replace(['.', '-', '#', '(', ')', '!'], ['\.', '\-', '\#', '\(', '\)', '\!'], $value['valor']); // Escapar los puntos con barra invertida
         $datos = [
             'chat_id' => $value['idtelegram'],
             //'chat_id' => 538709214,
-            'text' => $escapedMessage,
+            'text' => $si,
             'parse_mode' => 'MarkdownV2' #formato del mensaje
         ];
-    }elseif($value['tipo']=='documento'){
+    } elseif ($value['tipo'] == 'documento') {
         $datos = [
             'chat_id' => $value['idtelegram'],
             //'chat_id' => 538709214,
             'document' => $value['valor'],
             'caption' => $value['etiqueta']
         ];
-    }else{
+    } else {
         print_r("no cumple con tipo");
         break;
     }
 
     $ch = curl_init();
 
-    if($value['tipo']=='imagen'){
+    if ($value['tipo'] == 'imagen') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendPhoto");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -82,7 +100,7 @@ foreach ($fila as  $value) {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    }elseif($value['tipo']=='texto'){
+    } elseif ($value['tipo'] == 'texto') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendMessage");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -90,7 +108,7 @@ foreach ($fila as  $value) {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    }elseif($value['tipo']=='documento'){
+    } elseif ($value['tipo'] == 'documento') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendDocument");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -102,12 +120,12 @@ foreach ($fila as  $value) {
     }
 
     $r_array = json_decode(curl_exec($ch), true);
-    
+
     curl_close($ch);
-    if($r_array['ok'] == 1){
-        echo $value['idtelegram']," ","Campaña"," ",$value['id']," EXITOSO\n";
-    }else{
-        echo $value['idtelegram']," ","Campaña"," ",$value['id']," FALLO ", $r_array['error_code'], " ", $r_array['description'], "\n"  ;
+    if ($r_array['ok'] == 1) {
+        echo $value['idtelegram'], " ", "Campaña", " ", $value['id'], " EXITOSO\n";
+    } else {
+        echo $value['idtelegram'], " ", "Campaña", " ", $value['id'], " FALLO ", $r_array['error_code'], " ", $r_array['description'], "\n";
     }
 }
 
@@ -117,8 +135,7 @@ unset($value);
 <script>
     alert('Proceso terminado.');
     // Esperar 5 segundos y luego redireccionar
-    setTimeout(function() {
+    setTimeout(function () {
         window.location.href = 'carga.php';
     }, 1000); // 5000 milisegundos = 5 segundos
 </script>
-
--- programado.php
+++ programado.php
@@ -10,16 +10,19 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('America/Guatemala');
 
-if(!isset($_GET['tipo'])){
+if (!isset($_GET['tipo'])) {
     throw new Exception('Pais requerido');
 }
-$tipo=$_GET['tipo'];
+$tipo = $_GET['tipo'];
 
 $token = $TOKEN_BOT;
 
-$res ="select DISTINCT pc.*, vpb.idtelegram 
-from push_carga pc 
-join v_push_base vpb on pc.pais=vpb.pais
+$username = $_SESSION["username"];
+
+if ($username == "admin-ECONORED") {
+    $res = "select DISTINCT pc.*, vpb.idtelegram
+from push_carga pc
+join v_push_base_econored vpb on pc.pais=vpb.pais
 AND ((
       (pc.sucursal IS NULL OR pc.sucursal = '')
       AND (pc.zona IS NULL OR pc.zona = '')
@@ -31,40 +34,57 @@ AND ((
 where pc.programacion  = '$tipo' and pc.enviado is null
 ";
 
+} else {
+
+    $res = "select DISTINCT pc.*, vpb.idtelegram
+from push_carga pc
+join v_push_base vpb on pc.pais=vpb.pais
+AND ((
+      (pc.sucursal IS NULL OR pc.sucursal = '')
+      AND (pc.zona IS NULL OR pc.zona = '')
+	  AND (pc.ruta IS NULL OR pc.ruta = ''))
+        OR
+        ((pc.sucursal IS NOT NULL AND pc.sucursal != '' AND pc.sucursal = vpb.cod_sucursal)
+            OR (pc.zona     IS NOT NULL AND pc.zona     != '' AND pc.zona = vpb.zona)
+            OR (pc.ruta     IS NOT NULL AND pc.ruta     != '' AND pc.ruta = vpb.ruta)))
+where pc.programacion  = '$tipo' and pc.enviado is null
+";
+}
+
 $res = $mysqli->query("$res");
 $fila = $res->fetch_all(MYSQLI_ASSOC);
 
 
 
-foreach ($fila as  $value) {
+foreach ($fila as $value) {
 
-    if($value['tipo']=='imagen'){
+    if ($value['tipo'] == 'imagen') {
         $datos = [
             'chat_id' => $value['idtelegram'],
             'photo' => $value['valor'],
             'caption' => $value['etiqueta']
         ];
-    }elseif($value['tipo']=='texto'){
+    } elseif ($value['tipo'] == 'texto') {
         $escapedMessage = str_replace(['.', '-', '#', '(', ')', '!'], ['\.', '\-', '\#', '\(', '\)', '\!'], $value['valor']); // Escapar los puntos con barra invertida
         $datos = [
             'chat_id' => $value['idtelegram'],
             'text' => $escapedMessage,
             'parse_mode' => 'MarkdownV2' #formato del mensaje
         ];
-    }elseif($value['tipo']=='documento'){
+    } elseif ($value['tipo'] == 'documento') {
         $datos = [
             'chat_id' => $value['idtelegram'],
             'document' => $value['valor'],
             'caption' => $value['etiqueta']
         ];
-    }else{
+    } else {
         print_r("no cumple con tipo");
         break;
     }
 
     $ch = curl_init();
 
-    if($value['tipo']=='imagen'){
+    if ($value['tipo'] == 'imagen') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendPhoto");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -74,7 +94,7 @@ foreach ($fila as  $value) {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    }elseif($value['tipo']=='texto'){
+    } elseif ($value['tipo'] == 'texto') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendMessage");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -82,7 +102,7 @@ foreach ($fila as  $value) {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    }elseif($value['tipo']=='documento'){
+    } elseif ($value['tipo'] == 'documento') {
         curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendDocument");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -95,13 +115,13 @@ foreach ($fila as  $value) {
 
     $r_array = json_decode(curl_exec($ch), true);
     curl_close($ch);
-    if($r_array['ok'] == 1){
-        echo $value['idtelegram']," ","Campaña"," ",$value['id']," EXITOSO\n";
+    if ($r_array['ok'] == 1) {
+        echo $value['idtelegram'], " ", "Campaña", " ", $value['id'], " EXITOSO\n";
         $id = $value['id'];  // id de push_carga
         $update = "UPDATE push_carga SET enviado = 1 WHERE id = $id";
         $mysqli->query($update);
-    }else{
-        echo $value['idtelegram']," ","Campaña"," ",$value['id']," FALLO ", $r_array['error_code'], " ", $r_array['description'], "\n"  ;
+    } else {
+        echo $value['idtelegram'], " ", "Campaña", " ", $value['id'], " FALLO ", $r_array['error_code'], " ", $r_array['description'], "\n";
     }
 }
 
