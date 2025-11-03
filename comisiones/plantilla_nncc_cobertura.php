8d9420d2b9fe26b3a3894b51d42d950446c76ab1
bertol26 <bertolfranco26@gmail.com>
2025-11-03 12:10:52 -0500

    avance

--- comisiones/plantilla_nncc_cobertura.php
+++ comisiones/plantilla_nncc_cobertura.php
@@ -16,13 +16,23 @@ $active = "coberturanncc";
 // conexión
 
 if (isset($_POST["delete"])) {
-    $query = "DELETE FROM comisiones_foco WHERE pais = '".$paisSession."'";
+    $query = "DELETE FROM plantilla_cobertura_econored WHERE pais = '" . $paisSession . "'";
     $resultados = mysqli_query($mysqli, $query);
+
+    $uploadDir = __DIR__ . '/../assets/cajasminimas/';
+
+    $files = glob($uploadDir . '*'); // obtiene todos los archivos dentro de la carpeta
+
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file); // elimina el archivo
+        }
+    }
 
 }
 
 if (isset($_POST['enviar'])) {
-    $uploadDir = __DIR__ . '/../assets/';
+    $uploadDir = __DIR__ . '/../assets/cajasminimas/';
     $fileName = basename($_FILES['file']['name']);
     $fileTmp = $_FILES['file']['tmp_name'];
     $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
@@ -32,16 +42,24 @@ if (isset($_POST['enviar'])) {
     }
     $destPath = $uploadDir . $fileName;
     if (move_uploaded_file($fileTmp, $destPath)) {
-            echo "✅ Archivo guardado correctamente: uploads/$newFileName";
-        } else {
-            echo "❌ Error al subir el archivo.";
-        }
+        echo "✅ Archivo guardado correctamente: uploads/$newFileName";
+
+        $paisSession = $mysqli->real_escape_string($paisSession);
+        $fileName = $mysqli->real_escape_string($fileName);
+
+        $q = "INSERT INTO plantilla_cobertura_econored (pais, documento) VALUES ('$paisSession', '$fileName')";
+
+        $mysqli->query($q);
+    } else {
+        echo "❌ Error al subir el archivo.";
+    }
 }
 
 ?>
 
 <!doctype html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -56,180 +74,116 @@ if (isset($_POST['enviar'])) {
 </head>
 
 <body>
-<header>
-    <!-- Fixed navbar -->
-    <?php
-        if ($paisSession == "CO"){
+    <header>
+        <!-- Fixed navbar -->
+        <?php
+        if ($paisSession == "CO") {
             include "./comisiones_menu_co.php";
-        }
-        else{
+        } else {
             include "./comisiones_menu.php";
         } ?>
-</header>
+    </header>
 
-<!-- Begin page content -->
+    <!-- Begin page content -->
 
-<div class="container">
-    <div class="row align-items-start text-center">
-        <div class="col">
-            <h3 class="mt-3">Carga Plantilla Cobertura NNCC</h3>
+    <div class="container">
+        <div class="row align-items-start text-center">
+            <div class="col">
+                <h3 class="mt-3">Carga Plantilla Cobertura NNCC</h3>
+            </div>
+            <div class="col">
+                <img src="../ajetron.png" alt="Imagen de encabezado" class="img-fluid mt-3" style="max-width: 150px;">
+            </div>
         </div>
-        <div class="col">
-            <img src="../ajetron.png" alt="Imagen de encabezado" class="img-fluid mt-3" style="max-width: 150px;">
-        </div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-12 col-md-12">
-            <!-- Contenido -->
-            <div class="outer-container">
-                <form action="" method="post" name="frmExcelImport" id="frmExcelImport" enctype="multipart/form-data">
-                    <div>
-                        <div class="row">
+        <hr>
+        <div class="row">
+            <div class="col-12 col-md-12">
+                <!-- Contenido -->
+                <div class="outer-container">
+                    <form action="" method="post" name="frmExcelImport" id="frmExcelImport"
+                        enctype="multipart/form-data">
+                        <div>
+                            <div class="row">
 
-                            <div class="input-group mb-3">
-                                <label class="input-group-text" for="file">Elija archivo csv</label>
-                                <input type="file" class="form-control" name="file" id="file" accept=".csv">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="file">Elija archivo csv</label>
+                                    <input type="file" class="form-control" name="file" id="file" accept=".csv">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-                            <button type="submit" id="submit" name="enviar" class="btn btn-warning">Importar Registros
-                            </button>
-                            <button type="submit" id="submit" name="delete" class="btn btn-warning">Borrar Registros
-                            </button>
-
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
-                                        aria-expanded="false">
-                                    Descargar Plantillas
+                            <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                                <button type="submit" id="submit" name="enviar" class="btn btn-warning">Importar
+                                    Registros
                                 </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="static/1_plantilla_celula.csv">Celula</a></li>
-                                    <li><a class="dropdown-item" href="static/2_plantilla_banda.csv">Banda</a></li>
-                                    <li><a class="dropdown-item" href="static/8_plantilla_hitrate.csv">Hit Rate</a></li>
-                                    <li><a class="dropdown-item" href="static/3_plantilla_familias.csv">Familias</a></li>
-                                    <li><a class="dropdown-item" href="static/4_plantilla_tipo_comision.csv">Tipo Comision</a></li>
-                                    <li><a class="dropdown-item" href="static/5_plantilla_foco.csv">Foco</a></li>
-                                    <li><a class="dropdown-item" href="static/gt_cobertura_cliente_objetivo.csv">GT - Cobertura</a></li>
-                                </ul>
+                                <button type="submit" id="submit" name="delete" class="btn btn-warning">Borrar Registros
+                                </button>
                             </div>
+
                         </div>
+                    </form>
+                </div>
 
-                    </div>
-                </form>
-            </div>
-
-            <div class="outer-container" id="response" class="<?php if (!empty($type)) {
-                echo $type . " display-block";
-            } ?>"><?php if (!empty($message)) {
-                    echo $message;
-                } ?>
-            </div>
-            <br>
+                <div class="outer-container" id="response" class="<?php if (!empty($type)) {
+                    echo $type . " display-block";
+                } ?>"><?php if (!empty($message)) {
+                     echo $message;
+                 } ?>
+                </div>
+                <br>
 
 
-            <?php
-            $sqlSelect = "SELECT * FROM comisiones_foco where pais = '".$paisSession."'";
-            $result = mysqli_query($mysqli, $sqlSelect);
-
-            if (mysqli_num_rows($result) > 0) {
-                ?>
-
-                <table class='table table-bordered'>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Pais</th>
-                        <th>Articulo</th>
-                        <th>f1</th>
-                        <th>f2</th>
-                        <th>f3</th>
-                        <th>f4</th>
-                        <th>f5</th>
-                        <th>f6</th>
-                        <th>f7</th>
-                        <th>f8</th>
-                        <th>f9</th>
-                        <th>f10</th>
-                        <th>f11</th>
-                        <th>f12</th>
-                        <th>f13</th>
-                        <th>f14</th>
-                        <th>f15</th>
-                        <th>f16</th>
-                        <th>f17</th>
-                        <th>f18</th>
-                        <th>f19</th>
-                        <th>f20</th>
-                        <th>f21</th>
-                        <th>f22</th>
-                        <th>f23</th>
-                        <th>f24</th>
-                        <th>f25</th>
-                    </tr>
-                    </thead>
-                    <?php
-                    while ($row = mysqli_fetch_array($result)) {
-                    ?>
-                    <tbody>
-                    <tr>
-                        <td><?php echo $row['id']; ?></td>
-                        <td><?php echo $row['pais']; ?></td>
-                        <td><?php echo $row['articulo']; ?></td>
-                        <td><?php echo $row['f1']; ?></td>
-                        <td><?php echo $row['f2']; ?></td>
-                        <td><?php echo $row['f3']; ?></td>
-                        <td><?php echo $row['f4']; ?></td>
-                        <td><?php echo $row['f5']; ?></td>
-                        <td><?php echo $row['f6']; ?></td>
-                        <td><?php echo $row['f7']; ?></td>
-                        <td><?php echo $row['f8']; ?></td>
-                        <td><?php echo $row['f9']; ?></td>
-                        <td><?php echo $row['f10']; ?></td>
-                        <td><?php echo $row['f11']; ?></td>
-                        <td><?php echo $row['f12']; ?></td>
-                        <td><?php echo $row['f13']; ?></td>
-                        <td><?php echo $row['f14']; ?></td>
-                        <td><?php echo $row['f15']; ?></td>
-                        <td><?php echo $row['f16']; ?></td>
-                        <td><?php echo $row['f17']; ?></td>
-                        <td><?php echo $row['f18']; ?></td>
-                        <td><?php echo $row['f19']; ?></td>
-                        <td><?php echo $row['f20']; ?></td>
-                        <td><?php echo $row['f21']; ?></td>
-                        <td><?php echo $row['f22']; ?></td>
-                        <td><?php echo $row['f23']; ?></td>
-                        <td><?php echo $row['f24']; ?></td>
-                        <td><?php echo $row['f25']; ?></td>
-                    </tr>
-                    <?php
-                    }
-                    ?>
-                    </tbody>
-                </table>
                 <?php
-            }
-            ?>
-            <!-- Fin Contenido -->
+                $sqlSelect = "SELECT * FROM plantilla_cobertura_econored where pais = '" . $paisSession . "'";
+                $result = mysqli_query($mysqli, $sqlSelect);
+
+                if (mysqli_num_rows($result) > 0) {
+                    ?>
+
+                    <table class='table table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Pais</th>
+                                <th>Documento</th>
+                            </tr>
+                        </thead>
+                        <?php
+                        while ($row = mysqli_fetch_array($result)) {
+                            ?>
+                            <tbody>
+                                <tr>
+                                    <td><?php echo $row['id']; ?></td>
+                                    <td><?php echo $row['pais']; ?></td>
+                                    <td><?php echo $row['documento']; ?></td>
+                                </tr>
+                                <?php
+                        }
+                        ?>
+                        </tbody>
+                    </table>
+                    <?php
+                }
+                ?>
+                <!-- Fin Contenido -->
+            </div>
         </div>
+        <!-- Fin row -->
+
+
     </div>
-    <!-- Fin row -->
+    <!-- Fin container -->
+    <footer class="footer">
+        <div class="container"> <span class="text-muted">
+                <p>Todos los derechos reservados <a href="" target="_blank">AJEPER</a></p>
+            </span></div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
-
-</div>
-<!-- Fin container -->
-<footer class="footer">
-    <div class="container"> <span class="text-muted">
-    <p>Todos los derechos reservados <a href="" target="_blank">AJEPER</a></p>
-    </span></div>
-</footer>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-
-<!-- Bootstrap core JavaScript
+    <!-- Bootstrap core JavaScript
     ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
+    <!-- Placed at the end of the document so the pages load faster -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
