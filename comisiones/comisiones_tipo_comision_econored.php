c92b328b3e3e320f0711465ffd6bfe44daba52e7
bertol26 <bertolfranco26@gmail.com>
2025-11-20 16:08:46 -0500

    avance

---
+++ comisiones/comisiones_tipo_comision_econored.php
@@ -0,0 +1,240 @@
+<?php
+
+session_start();
+global $mysqli;
+require_once 'dbconect.php';
+
+if (!isset($_SESSION["username"])) {
+    header("Location: index.php");
+    exit();
+}
+clearstatcache();
+
+$paisSession = $_SESSION["pais"];
+$active = "tipo";
+// conexión
+
+if (isset($_POST["delete"])) {
+    $query = "DELETE FROM comisiones_tipo_econored WHERE pais = '".$paisSession."'";
+    $resultados = mysqli_query($mysqli, $query);
+
+}
+
+if (isset($_POST['enviar'])) {
+
+    $filename = $_FILES["file"]["name"];
+    $info = new SplFileInfo($filename);
+    $extension = pathinfo($info->getFilename(), PATHINFO_EXTENSION);
+
+    if ($extension == 'csv') {
+        $filename = $_FILES['file']['tmp_name'];
+        $handle = fopen($filename, "r");
+        $firstRowSkipped = false; // Añadir esta línea
+
+        // Obtener el delimitador seleccionado por el usuario
+        $selectedDelimiter = $_POST['delimiter'];
+
+//  $handle = str_replace(',',';',$handle);
+        while (($data = fgetcsv($handle, 100000, $selectedDelimiter)) !== FALSE) {
+
+            if (!$firstRowSkipped) {
+                $firstRowSkipped = true;
+                continue; // Saltar la primera fila
+            }
+
+            if( $paisSession == "HN" ) {
+                $q = "INSERT INTO comisiones_tipo_econored (pais,sistema,familia,tipodecomision,tipo,valor,valormin,valormax) VALUES (
+                            '$data[0]',
+                            '$data[1]',
+                            '$data[2]',
+                            '$data[3]',
+                            '$data[4]',
+                            '$data[5]',
+                            '$data[6]',
+                            '$data[7]'
+                            )";
+            }else {
+                $q = "INSERT INTO comisiones_tipo_econored (pais,sistema,familia,tipodecomision,tipo,valor) VALUES (
+                            '$data[0]',
+                            '$data[1]',
+                            '$data[2]',
+                            '$data[3]',
+                            '$data[4]',
+                            '$data[5]'
+                            )";
+            }
+
+
+            $mysqli->query($q);
+        }
+        fclose($handle);
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>Comisiones Carga Plantillas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+<header>
+    <!-- Fixed navbar -->
+    <?php
+        if ($paisSession == "CO"){
+            include "./comisiones_menu_co.php";
+        }
+        else{
+            include "./comisiones_menu.php";
+        } ?>
+</header>
+
+<!-- Begin page content -->
+
+<div class="container">
+    <div class="row align-items-start text-center">
+        <div class="col">
+            <h3 class="mt-3">Carga Plantilla Tipo Comision</h3>
+        </div>
+        <div class="col">
+            <img src="../ajetron.png" alt="Imagen de encabezado" class="img-fluid mt-3" style="max-width: 150px;">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-12 col-md-12">
+            <!-- Contenido -->
+            <div class="outer-container">
+                <form action="" method="post" name="frmExcelImport" id="frmExcelImport" enctype="multipart/form-data">
+                    <div>
+                        <div class="row">
+
+                            <div class="input-group mb-3">
+                                <label class="input-group-text" for="file">Elija archivo csv</label>
+                                <input type="file" class="form-control" name="file" id="file" accept=".csv">
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label" for="delimiter">Seleccione el delimitador: </label>
+                                <select class="form-select form-select-sm" aria-label="Default select example"
+                                        name="delimiter" id="delimiter">
+                                    <option value=",">Coma (,)</option>
+                                    <option value=";">Punto y coma (;)</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                            <button type="submit" id="submit" name="enviar" class="btn btn-warning">Importar Registros
+                            </button>
+                            <button type="submit" id="submit" name="delete" class="btn btn-warning">Borrar Registros
+                            </button>
+
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                    Descargar Plantillas
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="static/1_plantilla_celula.csv">Celula</a></li>
+                                    <li><a class="dropdown-item" href="static/2_plantilla_banda.csv">Banda</a></li>
+                                    <li><a class="dropdown-item" href="static/8_plantilla_hitrate.csv">Hit Rate</a></li>
+                                    <li><a class="dropdown-item" href="static/3_plantilla_familias.csv">Familias</a></li>
+                                    <li><a class="dropdown-item" href="static/4_plantilla_tipo_comision.csv">Tipo Comision</a></li>
+                                    <li><a class="dropdown-item" href="static/5_plantilla_foco.csv">Foco</a></li>
+                                    <li><a class="dropdown-item" href="static/gt_cobertura_cliente_objetivo.csv">GT - Cobertura</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+
+            <div class="outer-container" id="response" class="<?php if (!empty($type)) {
+                echo $type . " display-block";
+            } ?>"><?php if (!empty($message)) {
+                    echo $message;
+                } ?>
+            </div>
+            <br>
+
+
+            <?php
+            $sqlSelect = "SELECT * FROM comisiones_tipo_econored where pais = '".$paisSession."'";
+            $result = mysqli_query($mysqli, $sqlSelect);
+
+            if (mysqli_num_rows($result) > 0) {
+                ?>
+
+                <table class='table table-bordered'>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>pais</th>
+                        <th>sistema</th>
+                        <th>familia</th>
+                        <th>tipodecomision</th>
+                        <th>tipo</th>
+                        <th>valor</th>
+                        <?php
+                        if( $paisSession == "HN" ) { echo "<th>valormin</th><th>valormax</th>"; }
+                        ?>
+                    </thead>
+                    <?php
+                    while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <tbody>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['pais']; ?></td>
+                        <td><?php echo $row['sistema']; ?></td>
+                        <td><?php echo $row['familia']; ?></td>
+                        <td><?php echo $row['tipodecomision']; ?></td>
+                        <td><?php echo $row['tipo']; ?></td>
+                        <td><?php echo $row['valor']; ?></td>
+                        <td><?php if( $paisSession == "HN" ) { echo $row['valormin']; } ?></td>
+                        <td><?php if( $paisSession == "HN" ) { echo $row['valormax']; } ?></td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }
+            ?>
+            <!-- Fin Contenido -->
+        </div>
+    </div>
+    <!-- Fin row -->
+
+
+</div>
+<!-- Fin container -->
+<footer class="footer">
+    <div class="container"> <span class="text-muted">
+    <p>Todos los derechos reservados <a href="" target="_blank">AJEPER</a></p>
+    </span></div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
