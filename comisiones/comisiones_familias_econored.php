c1949a5edd5728c8dadb4f2debf4afc408a1c958
bertol26 <bertolfranco26@gmail.com>
2025-11-24 11:35:13 -0500

    avance

--- comisiones/comisiones_familias_econored.php
+++ comisiones/comisiones_familias_econored.php
@@ -150,12 +150,17 @@ if (isset($_POST['enviar'])) {
 <body>
 <header>
     <!-- Fixed navbar -->
-    <?php
+     <?php
+        $username = $_SESSION["username"];
         if ($paisSession == "CO"){
             include "./comisiones_menu_co.php";
         }
         else{
-            include "./comisiones_menu.php";
+            if ($username == "admin-ECONORED-CR" || $username == "admin-ECONORED-EC") {
+              include "./comisiones_menu_econored.php";
+            } else {
+              include "./comisiones_menu.php";
+            }
         } ?>
 </header>
 
