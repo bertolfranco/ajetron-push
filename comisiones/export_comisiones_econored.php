85a8f237dc4fa7af7bcd2a455c4c5d0b25c6c823
bertol26 <bertolfranco26@gmail.com>
2026-03-17 15:59:09 -0500

    commit

--- comisiones/export_comisiones_econored.php
+++ comisiones/export_comisiones_econored.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once 'dbconect.php';
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
